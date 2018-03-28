aedc52aa69ea7b23a08f0673a0be43d6c842d9e9
Pepi-Laptop <petroslav.angelov@gmail.com>
2018-03-28 16:41:08 +0300

    hopes and dreams

--- Test44/src/TryMan.java
+++ Test44/src/TryMan.java
@@ -1,7 +1,6 @@
 
 public class TryMan {
 
-<<<<<<< HEAD
 	public static void main(String[] args) throws InterruptedException {
 		//code
 		int mins = 1;
@@ -10,10 +9,5 @@ public class TryMan {
 			System.out.println(mins++ + " minutes have passed");
 			
 		}
-=======
-	public static void main(String[] args) {
-		System.out.println("Simple Test");
-		System.out.println("Test number two");
->>>>>>> d4ae26b93e37d2350eb468a0680773eb553f8dec
 	}
 }
