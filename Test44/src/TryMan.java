21236c521089da633fed5e5c3c8b54ce014df569
Pepi-Laptop <petroslav.angelov@gmail.com>
2018-03-28 16:20:33 +0300

    ranom min meter :D

--- Test44/src/TryMan.java
+++ Test44/src/TryMan.java
@@ -1,7 +1,13 @@
 
 public class TryMan {
 
-	public static void main(String[] args) {
-		System.out.println("Simple Test");
+	public static void main(String[] args) throws InterruptedException {
+		//code
+		int mins = 1;
+		while(true) {
+			Thread.sleep(60*1000);
+			System.out.println(mins++ + " minutes have passed");
+			
+		}
 	}
 }
