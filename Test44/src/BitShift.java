8f4478b7fe0082757540517158018e5b7b94fa5d
Pepi <petroslav.angelov@gmail.com>
2018-04-11 12:56:40 +0300

    foundations for a task kek

--- Test44/src/BitShift.java
+++ Test44/src/BitShift.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class BitShift {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+
+        int moves = sc.nextInt();
+        sc.nextLine();
+
+        String code = sc.nextLine();
+
+        int coef = Math.max(rows, cols);
+
+    }
 }
